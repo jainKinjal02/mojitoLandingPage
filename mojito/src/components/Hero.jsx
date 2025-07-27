2445023a6e91c7049893ff71bb6ff56cf1bbec01
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-27 16:40:10 +0530

    Navbar completed , setting up Hero section with

---
+++ mojito/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <section id="hero" className='noisy'>
+            <h1>class</h1>
+        </section>
+    </>
+  )
+}
+
+export default Hero
--- mojito/src/components/Navbar.jsx
+++ mojito/src/components/Navbar.jsx
@@ -1,6 +1,25 @@
-import { navLinks } from "../../constants"
+import { useGSAP } from "@gsap/react"
+import { navLinks } from "../../constants";
+import gsap from "gsap";
 
 const Navbar = () => {
+useGSAP(() => {
+    const navTween = gsap.timeline({
+        scrollTrigger: {
+            trigger: 'nav',
+            start: 'bottom top'
+        }
+    });
+
+    navTween.fromTo('nav' , {
+        backgroundColor:'transparent'},{
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blue(10px)',
+            duration: 1,
+            ease: 'power1.inOut'   
+    });
+})
+
   return (
     <nav>
         <div>
