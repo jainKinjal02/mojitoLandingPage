1d8da477502021f418d96dd1f985679272990fcc
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-28 22:15:29 +0530

    Introducing Art section

---
+++ mojito/src/components/Art.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Art = () => {
+  return (
+    <div id="art">
+        <div className='container mx-auto h-full pt-20'>
+            <h2 className='will-fade'>The ART</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Art
