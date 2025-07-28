9e7bed4c8c1ed79425665b2c4846e537afa0c2f0
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-28 22:18:51 +0530

    Art section - part 1

--- mojito/src/components/Art.jsx
+++ mojito/src/components/Art.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { goodLists } from '../../constants'
 
 const Art = () => {
   return (
     <div id="art">
         <div className='container mx-auto h-full pt-20'>
             <h2 className='will-fade'>The ART</h2>
+
+            <div className='content'>
+                <ul className='space-y-4 will-fade'>
+                    {goodLists.map((feature,index)=> (
+                        <li key={index} className='flex intems-center gap-2'>
+                            <img src="/images/check.png" alt="check" />
+                            <p>{feature}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     </div>
   )
