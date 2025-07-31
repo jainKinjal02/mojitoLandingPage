9a65324bdd3b4a20a3a8b9b38e6395a3957e0ebf
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-31 22:20:52 +0530

    Setting up the footer section

---
+++ mojito/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+        <img src="/images/footer-right-leaf.png"  alt="right-leaf" id="f-right-leaf" />
+        <img src="/images/footer-left-leaf.png"  alt="left-leaf" id="f-left-leaf" />
+    </footer>
+  )
+}
+
+export default Contact
