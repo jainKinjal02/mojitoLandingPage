3bef49f671d66579a1e834b3c7cba39e46e098b2
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-31 22:17:52 +0530

    Menu section animated

--- mojito/src/components/Menu.jsx
+++ mojito/src/components/Menu.jsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState , useRef} from 'react'
 import { allCocktails } from '../../constants'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Menu = () => {
+    const contentRef = useRef();
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    useGSAP(()=> {
+        gsap.fromTo('#title', {opacity: 0},{opacity: 1, duration: 1} );
+        gsap.fromTo('.cocktail img', {opacity: 0, xPercent: -100}, {
+            xPercent: 0, opacity: 1, duration: 1, ease: 'power1.inOut'
+
+        })
+        gsap.fromTo('.details h2', {yPercent: 100, opacity: 0}, {
+            yPercent: 0, opacity: 100, ease: "power1.inOut"
+
+        })
+        gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {
+            yPercent: 0, opacity: 100, ease: "power1.inOut"
+
+        })
+    },[currentIndex]);
     const totalCocktails = allCocktails.length;
 
     const goToSlide = (index) => {
@@ -10,6 +29,14 @@ const Menu = () => {
 
         setCurrentIndex(newIndex);
     }
+
+    const getCocktailAt = (indexOffset) => {
+        return allCocktails[(currentIndex + indexOffset + totalCocktails) % totalCocktails]
+    }
+
+    const currentCocktail = getCocktailAt(0);
+    const prevCocktail = getCocktailAt(-1);
+    const nextCocktail = getCocktailAt(1);
 
   return (
     <section id="menu" aria-labelledby='menu-heading'>
@@ -39,24 +66,36 @@ const Menu = () => {
         </nav>
 
         <div className='content'>
-            <div className='arrow'>
+            <div className='arrows'>
                 <button className='text-left' onClick={()=> goToSlide(currentIndex - 1)}>
                     <span>
-                        prevcocktainname
+                        {prevCocktail.name}
                     </span>
                     <img src="/images/right-arrow.png" alt="right-arrow" aria-hidden="true" />
                 </button>
 
                 <button className='text-left' onClick={()=> goToSlide(currentIndex + 1)}>
                     <span>
-                        nextCocktailname
+                        {nextCocktail.name}
                     </span>
                     <img src="/images/left-arrow.png" alt="left-arrow" aria-hidden="true" />
                 </button>
             </div>
 
-            <div>
-                
+            <div className='cocktail'>
+                <img src={currentCocktail.image} className='object-contain'/>
+            </div>
+
+            <div className='recipe'>
+                <div ref={contentRef} className='info'>
+                    <p>Recipe for:</p>
+                    <p id="title">{currentCocktail.name}</p>
+                </div>
+
+                <div className='details'>
+                    <h2>{currentCocktail.title}</h2>
+                    <p>{currentCocktail.description}</p>
+                </div>
             </div>
         </div>
     </section>
