ac2a2571f3c2a7950d6292727d89fafbc55363bc
Kinjal Jain <jainkinjal021196@gmail.com>
2025-07-31 21:58:09 +0530

    Adding  the logic for slider

--- mojito/src/components/Menu.jsx
+++ mojito/src/components/Menu.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { allCocktails } from '../../constants'
 
 const Menu = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const totalCocktails = allCocktails.length;
+
+    const goToSlide = (index) => {
+        const newIndex = (index + totalCocktails) % totalCocktails;
+
+        setCurrentIndex(newIndex);
+    }
+
   return (
     <section id="menu" aria-labelledby='menu-heading'>
         <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf"/>
@@ -13,9 +22,43 @@ const Menu = () => {
 
         <nav className='cocktail-tabs' aria-label='Cocktail Navigation'>
             {allCocktails.map((cocktail,index)=> {
-                const isActive = index === currentIndex
+                const isActive = index === currentIndex;
+
+                return (
+                    <button key={cocktail.id} className={`
+                        ${isActive 
+                        ? 'text-white border-white'
+                        : 'text-white/50 border-white/50'}
+                    `}
+                            onClick={() => goToSlide(index)}
+                    >
+                        {cocktail.name}
+                    </button>
+                )
             })}
         </nav>
+
+        <div className='content'>
+            <div className='arrow'>
+                <button className='text-left' onClick={()=> goToSlide(currentIndex - 1)}>
+                    <span>
+                        prevcocktainname
+                    </span>
+                    <img src="/images/right-arrow.png" alt="right-arrow" aria-hidden="true" />
+                </button>
+
+                <button className='text-left' onClick={()=> goToSlide(currentIndex + 1)}>
+                    <span>
+                        nextCocktailname
+                    </span>
+                    <img src="/images/left-arrow.png" alt="left-arrow" aria-hidden="true" />
+                </button>
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
     </section>
   )
 }
