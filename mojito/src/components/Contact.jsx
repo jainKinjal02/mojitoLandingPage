ad47d4fed330cd6178ffefd79a6487c04faa9c5b
Kinjal Jain <jainkinjal021196@gmail.com>
2025-08-03 18:37:03 +0530

    Setting up Contact Section

--- mojito/src/components/Contact.jsx
+++ mojito/src/components/Contact.jsx
@@ -5,6 +5,14 @@ const Contact = () => {
     <footer id="contact">
         <img src="/images/footer-right-leaf.png"  alt="right-leaf" id="f-right-leaf" />
         <img src="/images/footer-left-leaf.png"  alt="left-leaf" id="f-left-leaf" />
+
+        <div className='content'>
+            <h2>Where to Find Us</h2>
+
+            <div>
+                <h3>Visit Out Store</h3>
+            </div>
+        </div>
     </footer>
   )
 }
